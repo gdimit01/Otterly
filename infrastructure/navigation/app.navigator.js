ae5216407c0d445aa41bedad3ec09c5c1e1502bf
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-07-29 11:41:14 +0100

    correctly modularised navigations

---
+++ infrastructure/navigation/app.navigator.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { WelcomeNavigator } from "./welcome.navigator";
+import { HomeNavigator } from "./home.navigator";
+import EventScreen from "../../screens/EventScreen";
+import StudyGroupsScreen from "../../screens/StudyGroupsScreen";
+import SocialGroupsScreen from "../../screens/SocialGroupsScreen";
+
+const Stack = createNativeStackNavigator();
+
+export function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="WelcomeNavigator">
+        <Stack.Screen
+          name="WelcomeNavigator"
+          component={WelcomeNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="HomeStack" // Changed this from "HomeNavigator" to "HomeStack"
+          component={HomeNavigator}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="EventScreen"
+          component={EventScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="StudyGroups"
+          component={StudyGroupsScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SocialGroups"
+          component={SocialGroupsScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ infrastructure/navigation/welcome.navigator.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import WelcomeScreen from "../../screens/WelcomeScreen";
+import LoginScreen from "../../screens/LoginScreen";
+import SignupScreen from "../../screens/SignupScreen";
+
+const Stack = createNativeStackNavigator();
+
+export const WelcomeNavigator = () => {
+  return (
+    <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="SignUp" component={SignupScreen} />
+    </Stack.Navigator>
+  );
+};
