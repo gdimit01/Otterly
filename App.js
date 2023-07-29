9001b0fd8e82fbf4be9cbe25a0969cf55b25a22c
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-07-29 12:15:36 +0100

    app update

--- App.js
+++ App.js
@@ -1,96 +1,11 @@
 import React from "react";
 import { EventProvider } from "./screens/EventContext";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Ionicons } from "@expo/vector-icons"; // make sure to install this package
-//import { UserProvider } from "./screens/UserProvider"; // Update this import path to the actual location of your UserProvider component
-
-import HomeScreen from "./screens/HomeScreen";
-import ExploreScreen from "./screens/ExploreScreen";
-import CreateEventScreen from "./screens/CreateEventScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import NotificationsScreen from "./screens/NotificationsScreen";
-import EventScreen from "./screens/EventScreen";
-import StudyGroupsScreen from "./screens/StudyGroupsScreen";
-import SocialGroupsScreen from "./screens/SocialGroupsScreen";
-import SignupScreen from "./screens/SignupScreen";
-import LoginScreen from "./screens/LoginScreen";
-import WelcomeScreen from "./screens/WelcomeScreen";
-
-const Tab = createBottomTabNavigator();
-const Stack = createNativeStackNavigator();
-
-function HomeTabs() {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          const icons = {
-            Home: focused ? "home" : "home-outline",
-            Explore: focused ? "compass" : "compass-outline",
-            Create: focused ? "add" : "add-outline",
-            Notifications: focused ? "alarm" : "alarm-outline",
-            Profile: focused ? "person" : "person-outline",
-          };
-
-          // You can return any component that you like here!
-          return (
-            <Ionicons name={icons[route.name]} size={size} color={color} />
-          );
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: "tomato",
-        inactiveTintColor: "gray",
-      }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ headerShown: false }}
-      />
-      <Tab.Screen
-        name="Explore"
-        component={ExploreScreen}
-        options={{ headerShown: false }}
-      />
-      <Tab.Screen
-        name="Create"
-        component={CreateEventScreen}
-        options={{ headerShown: false }}
-      />
-      <Tab.Screen
-        name="Notifications"
-        component={NotificationsScreen}
-        options={{ headerShown: false }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{ headerShown: false }}
-      />
-    </Tab.Navigator>
-  );
-}
+import { AppNavigator } from "./infrastructure/navigation/app.navigator"; // Import the AppNavigator
 
 export default function App() {
   return (
     <EventProvider>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Welcome"
-          screenOptions={{ headerShown: false }}
-        >
-          <Stack.Screen name="Welcome" component={WelcomeScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Home" component={HomeTabs} />
-          <Stack.Screen name="SignUp" component={SignupScreen} />
-          <Stack.Screen name="EventScreen" component={EventScreen} />
-          <Stack.Screen name="StudyGroups" component={StudyGroupsScreen} />
-          <Stack.Screen name="SocialGroups" component={SocialGroupsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <AppNavigator /> {/* Use the AppNavigator component */}
     </EventProvider>
   );
 }
