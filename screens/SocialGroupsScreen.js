76ac7ee344e5c561fb91d32924b9507d964b7a6e
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-07-29 11:58:58 +0100

    all the navigation stuff

--- screens/SocialGroupsScreen.js
+++ screens/SocialGroupsScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import {
   View,
   Text,
@@ -9,24 +9,13 @@ import {
   SafeAreaView,
 } from "react-native";
 
-import SearchBarComponent from "../components/SearchBarComponent";
-import { EventContext } from "../screens/EventContext"; // Import EventContext
-
-const SocialGroupsScreen = () => {
-  const { events } = useContext(EventContext); // Use EventContext
-
-  // Check if events is defined before calling filter
-  const socialGroupEvents = events
-    ? events.filter((event) => event.group === "SocialGroup")
-    : [];
-
+export default function SocialGroupsScreen() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar barStyle="dark-content" />
-      <Text style={styles.title}>Social Groups</Text>
-      <SearchBarComponent />
+      <Text style={styles.title}>Study Groups</Text>
       <FlatList
-        data={socialGroupEvents} // Use socialGroupEvents instead of DATA
+        data={DATA}
         renderItem={({ item }) => (
           <Card name={item.name} location={item.location} image={item.image} />
         )}
@@ -34,7 +23,7 @@ const SocialGroupsScreen = () => {
       />
     </SafeAreaView>
   );
-};
+}
 
 const Card = ({ name, location, image }) => (
   <View style={styles.card}>
@@ -43,6 +32,28 @@ const Card = ({ name, location, image }) => (
     <Text style={styles.location}>{location}</Text>
   </View>
 );
+
+const DATA = [
+  {
+    id: "1",
+    name: "React Native Austin",
+    location: "Austin, USA",
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: "2",
+    name: "React Native Bangalore",
+    location: "Bangalore, India",
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    id: "3",
+    name: "React Native Montevideo",
+    location: "Montevideo, Uruguay",
+    image: "https://via.placeholder.com/150",
+  },
+  // Add more groups here...
+];
 
 const styles = StyleSheet.create({
   title: {
@@ -73,4 +84,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default SocialGroupsScreen;
+//export default SocialGroupsScreen;
