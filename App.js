27e8a382c53709f103c244793e13ebea8c4c2215
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-07-30 22:34:36 +0100

    now android can click and drag notification cards

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { EventProvider } from "./screens/EventContext";
 import { AppNavigator } from "./infrastructure/navigation/app.navigator"; // Import the AppNavigator
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <EventProvider>
-      <AppNavigator />
-    </EventProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <EventProvider>
+        <AppNavigator />
+      </EventProvider>
+    </GestureHandlerRootView>
   );
 }
