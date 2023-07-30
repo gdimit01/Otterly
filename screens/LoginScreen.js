f84ecea74ae7db932cdf3718d1dcda69300a5593
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-07-30 22:24:32 +0100

    fixed android forgot password

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,5 @@
 import { useNavigation } from "@react-navigation/core";
+import Dialog from "react-native-dialog";
 import React, { useEffect, useState } from "react";
 import { signInWithEmailAndPassword } from "@firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -23,6 +24,8 @@ import FormInput from "../components/FormInput";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { sendPasswordResetEmail } from "firebase/auth"; //the key
 const LoginScreen = () => {
+  const [dialogVisible, setDialogVisible] = useState(false);
+  const [resetEmail, setResetEmail] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
@@ -61,69 +64,109 @@ const LoginScreen = () => {
       });
   };
 
+  const showDialog = () => {
+    setDialogVisible(true);
+  };
+
+  const handleCancel = () => {
+    setDialogVisible(false);
+  };
+
+  const handleOk = () => {
+    if (resetEmail) {
+      sendPasswordResetEmail(auth, resetEmail)
+        .then(() => {
+          Alert.alert("Password reset email sent!");
+        })
+        .catch((error) => {
+          Alert.alert(
+            "Error occurred while sending password reset email: ",
+            error.message
+          );
+        });
+    } else {
+      Alert.alert("Please enter your email.");
+    }
+    setDialogVisible(false);
+  };
+
   const handleForgotPassword = () => {
-    Alert.prompt(
-      "Forgot Password",
-      "Please enter your email:",
-      [
-        {
-          text: "Cancel",
-          style: "cancel",
-        },
-        {
-          text: "OK",
-          onPress: (email) => {
-            if (email) {
-              sendPasswordResetEmail(auth, email)
-                .then(() => {
-                  Alert.alert("Password reset email sent!");
-                })
-                .catch((error) => {
-                  Alert.alert(
-                    "Error occurred while sending password reset email: ",
-                    error.message
-                  );
-                });
-            } else {
-              Alert.alert("Please enter your email.");
-            }
+    if (Platform.OS === "android") {
+      setResetEmail("");
+      showDialog();
+    } else {
+      Alert.prompt(
+        "Forgot Password",
+        "Please enter your email:",
+        [
+          {
+            text: "Cancel",
+            style: "cancel",
           },
-        },
-      ],
-      "plain-text"
-    );
+          {
+            text: "OK",
+            onPress: (email) => {
+              if (email) {
+                sendPasswordResetEmail(auth, email)
+                  .then(() => {
+                    Alert.alert("Password reset email sent!");
+                  })
+                  .catch((error) => {
+                    Alert.alert(
+                      "Error occurred while sending password reset email: ",
+                      error.message
+                    );
+                  });
+              } else {
+                Alert.alert("Please enter your email.");
+              }
+            },
+          },
+        ],
+        "plain-text"
+      );
+    }
   };
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <KeyboardAvoidingView style={styles.container} behavior="padding">
-        <StatusBar barStyle="dark-content" />
-        {/* Add StatusBar for both Android and iOS */}
-        <View style={styles.container}>
-          <Icon name="user" size={30} color="#000" style={styles.icon} />
-          <Text style={styles.title}>Otterly App</Text>
-          <FormInput
-            value={email}
-            placeholder="Email"
-            onChangeText={(text) => setEmail(text)}
-          />
-          <FormInput
-            value={password}
-            placeholder="Password"
-            secure={true}
-            onChangeText={(text) => setPassword(text)}
-          />
-          {loading ? (
-            <ActivityIndicator size="large" color="#f64060" />
-          ) : (
-            <FormButton title="Log In" onPress={handleLogin} />
-          )}
-          <Text style={styles.forgotPassword} onPress={handleForgotPassword}>
-            Forgot Password?
-          </Text>
-        </View>
-      </KeyboardAvoidingView>
-    </TouchableWithoutFeedback>
+    <>
+      <Dialog.Container visible={dialogVisible}>
+        <Dialog.Title>Forgot Password</Dialog.Title>
+        <Dialog.Description>Please enter your email:</Dialog.Description>
+        <Dialog.Input onChangeText={(text) => setResetEmail(text)} />
+        <Dialog.Button label="Cancel" onPress={handleCancel} />
+        <Dialog.Button label="OK" onPress={handleOk} />
+      </Dialog.Container>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView style={styles.container} behavior="padding">
+          <StatusBar barStyle="dark-content" />
+          {/* Add StatusBar for both Android and iOS */}
+          <View style={styles.container}>
+            <Icon name="user" size={30} color="#000" style={styles.icon} />
+            <Text style={styles.title}>Otterly App</Text>
+            <FormInput
+              value={email}
+              placeholder="Email"
+              onChangeText={(text) => setEmail(text)}
+            />
+            <FormInput
+              value={password}
+              placeholder="Password"
+              secure={true}
+              onChangeText={(text) => setPassword(text)}
+            />
+            {loading ? (
+              <ActivityIndicator size="large" color="#f64060" />
+            ) : (
+              <FormButton title="Log In" onPress={handleLogin} />
+            )}
+            <Text style={styles.forgotPassword} onPress={handleForgotPassword}>
+              Forgot Password?
+            </Text>
+          </View>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
+    </>
   );
 };
 
