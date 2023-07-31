34c05e603bd2e6d6c41da5a3ac9ed30eafb3cdd9
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-07-31 09:52:47 +0100

    dealt with annoying console log

--- App.js
+++ App.js
@@ -3,6 +3,14 @@ import { EventProvider } from "./screens/EventContext";
 import { AppNavigator } from "./infrastructure/navigation/app.navigator"; // Import the AppNavigator
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
+// import * as Device from "expo-device";
+
+// console.log(Device.modelName);
+
+import { LogBox } from "react-native";
+
+LogBox.ignoreLogs(["Constants.platform.ios.model has been deprecated"]);
+
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
