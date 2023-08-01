2fa1d2d84a60f6042ba2aa5126dd57f0a10c05a3
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-08-01 19:29:42 +0100

    updated studygroupsscreen styles

--- screens/StudyGroupsScreen.js
+++ screens/StudyGroupsScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -8,125 +8,145 @@ import {
   StatusBar,
   SafeAreaView,
   TouchableOpacity,
-  ActivityIndicator,
 } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { EventContext } from "../screens/EventContext"; // Updated
+import { useNavigation, useIsFocused } from "@react-navigation/native";
 import {
   getFirestore,
   collection,
-  getDocs,
+  onSnapshot,
   query,
   where,
-} from "firebase/firestore"; // Updated
-import SearchBarComponent from "../components/SearchBarComponent";
+  doc,
+  deleteDoc,
+} from "@firebase/firestore";
 
-const StudyGroupsScreen = () => {
+const StudyGroupsCard = ({
+  id,
+  title,
+  description,
+  image,
+  time,
+  group,
+  tag,
+}) => {
   const navigation = useNavigation();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [events, setEvents] = useContext(EventContext);
-  const [searchQuery, setSearchQuery] = useState("");
 
-  useEffect(() => {
-    setLoading(true);
+  // Function to delete study group
+  const deleteStudyGroup = async () => {
     const db = getFirestore();
-    const fetchEvents = async () => {
-      try {
-        const q = query(
-          collection(db, "events"),
-          where("group", "==", "Study Group")
-        );
-        const querySnapshot = await getDocs(q);
-        let eventsData = [];
-        querySnapshot.forEach((doc) => {
-          eventsData.push({ id: doc.id, ...doc.data() });
-        });
-        setEvents(eventsData);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchEvents();
-  }, []);
-
-  const filteredEvents = events.filter((event) =>
-    event.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  const handleEventPress = (event) => {
-    navigation.navigate("EventScreen", {
-      image: event.image,
-      title: event.name,
-      description: event.description,
-      time: event.time,
-      location: event.location,
-      tag: event.tag,
-      group: event.group,
-    });
+    await deleteDoc(doc(db, "studygroups", id));
   };
 
-  if (loading) {
-    return (
-      <View style={styles.loadingContainer}>
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
-    );
-  }
+  return (
+    <View style={styles.card}>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        onPress={() => {
+          navigation.navigate("EventScreen", {
+            id,
+            title,
+            description,
+            image,
+            time,
+            group,
+            tag,
+          });
+        }}
+      >
+        <Image source={{ uri: image }} style={styles.image} />
+        <View style={styles.text}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.description}>{description}</Text>
+          <Text style={styles.time}>{time}</Text>
+          <Text style={styles.group}>{group}</Text>
+          <Text style={styles.tag}>#{tag}</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        onPress={deleteStudyGroup}
+        style={styles.deleteButton}
+      >
+        <Text style={styles.deleteText}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-  if (error) {
-    return <Text>Error: {error}</Text>;
-  }
+const StudyGroupsScreen = () => {
+  const isFocused = useIsFocused();
+  const [studygroups, setStudyGroups] = useState([]);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (isFocused) {
+      const db = getFirestore();
+      const q = query(
+        collection(db, "studygroups"),
+        where("group", "==", "Study Group")
+      );
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        let studyGroupsData = [];
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          let formattedTime = "";
+          if (typeof data.time === "string") {
+            let parts = data.time.split(/[\s/:,]+/);
+            let date = new Date(
+              Date.UTC(
+                parts[2],
+                parts[1] - 1,
+                parts[0],
+                parts[3],
+                parts[4],
+                parts[5]
+              )
+            );
+            if (isNaN(date)) {
+              console.error("Could not parse date string:", data.time);
+            } else {
+              formattedTime = date.toLocaleString();
+            }
+          }
+
+          studyGroupsData.push({ id: doc.id, ...data, time: formattedTime });
+        });
+        setStudyGroups(studyGroupsData);
+      });
+
+      return () => unsubscribe();
+    }
+  }, [isFocused]);
+
+  const renderItem = ({ item }) => (
+    <StudyGroupsCard
+      id={item.id}
+      title={item.name} // Pass the name property for title
+      description={item.description}
+      image={item.image}
+      time={item.time}
+      group={item.group} // Pass the group property
+      tag={item.tag} // Pass the tag property
+    />
+  );
 
   return (
     <SafeAreaView style={{ flex: 1, paddingTop: StatusBar.currentHeight }}>
       <StatusBar barStyle="dark-content" />
-      <Text style={styles.title}>Study Groups</Text>
-      <SearchBarComponent
-        searchQuery={searchQuery}
-        setSearchQuery={setSearchQuery}
-      />
-      <FlatList
-        data={filteredEvents}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => handleEventPress(item)}>
-            <View style={styles.card}>
-              <Image
-                style={styles.image}
-                source={{ uri: "https://via.placeholder.com/150" }}
-              />
-              <Text style={styles.name}>{item.name}</Text>
-              <Text style={styles.location}>{item.location}</Text>
-              <Text style={styles.time}>Time: {item.time}</Text>
-              <Text style={styles.group}>Group: {item.group}</Text>
-            </View>
-          </TouchableOpacity>
-        )}
-        keyExtractor={(item) => item.id}
-      />
+      <View style={styles.content}>
+        <Text style={styles.title}>Study Groups</Text>
+        <FlatList
+          data={studygroups}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          contentContainerStyle={{ paddingBottom: 40 }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  loadingContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    margin: 10,
-  },
-  time: {
-    fontSize: 16,
-    color: "gray",
-    marginTop: 5,
-  },
   card: {
     flex: 1,
     margin: 10,
@@ -148,10 +168,61 @@ const styles = StyleSheet.create({
     color: "gray",
     marginTop: 5,
   },
-  group: {
+  deleteAction: {
+    backgroundColor: "#dd2c00",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 70,
+    height: "100%",
+    flexDirection: "row",
+  },
+  actionText: {
+    color: "#fff",
+    fontWeight: "600",
+    padding: 20,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    margin: 10,
+  },
+  text: {
+    flex: 1,
+  },
+  description: {
     fontSize: 16,
-    color: "blue",
     marginTop: 5,
+  },
+  time: {
+    fontSize: 14,
+    marginTop: 5,
+    color: "#888",
+  },
+  group: {
+    color: "#0000FF", // Default blue color
+    fontSize: 12,
+    fontWeight: "bold",
+    position: "absolute", // Position it absolutely
+  },
+  tag: {
+    color: "#0000FF", // Default blue color
+    fontSize: 12,
+    fontWeight: "bold",
+  },
+  deleteButton: {
+    backgroundColor: "#ff0000",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  deleteText: {
+    color: "#ffffff",
+    textAlign: "center",
   },
 });
 
