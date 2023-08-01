bbc913bdc48288dc20e132a72b8618158eec7f0e
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-08-01 20:40:19 +0100

    minor update

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,7 +1,7 @@
 import React from "react";
+import Icon from "react-native-vector-icons/Ionicons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Icon from "react-native-vector-icons/Ionicons";
 
 import HomeNavigator from "./HomeNavigator";
 import MeetUpsNavigator from "./MeetUpsNavigator";
