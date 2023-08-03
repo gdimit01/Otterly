548322bf310f5b0dfbf7e140c7b5a0160c205fd1
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-08-03 23:01:46 +0100

    slowly building up the invitations

--- src/components/SocialGroups/SocialGroupsCard.js
+++ src/components/SocialGroups/SocialGroupsCard.js
@@ -5,7 +5,7 @@ import {
   Image,
   TouchableOpacity,
   StyleSheet,
-  Alert, // Add this line
+  Alert,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import {
@@ -29,23 +29,49 @@ const SocialGroupsCard = ({
   time,
   group,
   tag,
-  visibility, // Add visibility prop
+  visibility,
 }) => {
   const [UserRSVP, setUserRSVP] = useState(false);
+  const [likes, setLikes] = useState(0);
+  const [userLiked, setUserLiked] = useState(false);
   const navigation = useNavigation();
 
-  // Function to delete social group
+  useEffect(() => {
+    const db = getFirestore();
+    const likesRef = collection(db, "socialgroups", id, "likes");
+    const unsubscribeLikes = onSnapshot(likesRef, (snapshot) => {
+      setLikes(snapshot.size);
+      setUserLiked(
+        snapshot.docs.map((doc) => doc.id).includes(auth.currentUser.uid)
+      );
+    });
+
+    const rsvpRef = collection(db, "socialgroups", id, "rsvps");
+    const currentUser = auth.currentUser;
+    if (currentUser) {
+      const unsubscribeRSVP = onSnapshot(
+        doc(rsvpRef, currentUser.uid),
+        (snapshot) => {
+          setUserRSVP(snapshot.exists());
+        }
+      );
+
+      return () => {
+        unsubscribeLikes();
+        unsubscribeRSVP();
+      };
+    }
+
+    return () => {
+      unsubscribeLikes();
+    };
+  }, [id]);
+
   const deleteSocialGroup = async () => {
     const db = getFirestore();
     await deleteDoc(doc(db, "socialgroups", id));
   };
 
-  const [likes, setLikes] = useState(0); // State to track likes
-
-  // Initialize a state to track if the current user has liked the post
-  const [userLiked, setUserLiked] = useState(false);
-
-  // Function to handle likes
   const handleLikes = async () => {
     const db = getFirestore();
     const socialGroupRef = doc(db, "socialgroups", id);
@@ -56,7 +82,6 @@ const SocialGroupsCard = ({
       const data = socialGroupSnapshot.data();
 
       if (data.creator.uid === currentUser.uid) {
-        // Prevent the creator from liking their own post
         Alert.alert("Error", "You can't like your own post.");
         return;
       }
@@ -65,50 +90,15 @@ const SocialGroupsCard = ({
       const userLikeSnapshot = await getDoc(doc(likesRef, currentUser.uid));
 
       if (!userLikeSnapshot.exists()) {
-        // User hasn't liked yet, let's add a like
         await setDoc(doc(likesRef, currentUser.uid), { uid: currentUser.uid });
         setUserLiked(true);
       } else {
-        // User already liked, remove like
         await deleteDoc(doc(likesRef, currentUser.uid));
         setUserLiked(false);
       }
-
-      const newLikesSnapshot = await getDocs(likesRef);
-      setLikes(newLikesSnapshot.size);
     }
   };
 
-  // Function to reset likes
-  const resetLikes = async () => {
-    const db = getFirestore();
-    const likesRef = collection(db, "socialgroups", id, "likes");
-    const likesSnapshot = await getDocs(likesRef);
-
-    likesSnapshot.docs.forEach(async (docSnapshot) => {
-      await deleteDoc(doc(db, "socialgroups", id, "likes", docSnapshot.id));
-    });
-
-    setLikes(0);
-  };
-
-  useEffect(() => {
-    // Add a listener for changes in the number of likes
-    const db = getFirestore();
-    const likesRef = collection(db, "socialgroups", id, "likes");
-    const unsubscribe = onSnapshot(likesRef, (snapshot) => {
-      setLikes(snapshot.size);
-      setUserLiked(
-        snapshot.docs.map((doc) => doc.id).includes(auth.currentUser.uid)
-      );
-    });
-
-    // Cleanup the listener on component unmount
-    return () => unsubscribe();
-  }, [id]);
-
-  // Function to handle RSVP
-  // Inside SocialGroupsCard component
   const handleRSVP = async () => {
     const db = getFirestore();
     const socialGroupRef = doc(db, "socialgroups", id);
@@ -119,42 +109,13 @@ const SocialGroupsCard = ({
       const userRsvpSnapshot = await getDoc(doc(rsvpRef, currentUser.uid));
 
       if (!userRsvpSnapshot.exists()) {
-        // User hasn't RSVPed yet, let's add an RSVP
         await setDoc(doc(rsvpRef, currentUser.uid), { uid: currentUser.uid });
       } else {
-        // User already RSVPed, remove RSVP
         await deleteDoc(doc(rsvpRef, currentUser.uid));
       }
     }
   };
-  // Inside SocialGroupsCard component
-  useEffect(() => {
-    const db = getFirestore();
-    const likesRef = collection(db, "socialgroups", id, "likes");
-    const rsvpRef = collection(db, "socialgroups", id, "rsvps");
-    const unsubscribeLikes = onSnapshot(likesRef, (snapshot) => {
-      setLikes(snapshot.size);
-    });
 
-    const currentUser = auth.currentUser;
-    if (currentUser) {
-      const unsubscribeRSVP = onSnapshot(
-        doc(rsvpRef, currentUser.uid),
-        (snapshot) => {
-          setUserRSVP(snapshot.exists());
-        }
-      );
-      return () => {
-        unsubscribeRSVP();
-      };
-    }
-
-    return () => {
-      unsubscribeLikes();
-    };
-  }, [id]);
-
-  // Function to toggle visibility of the event
   const toggleVisibility = async () => {
     const db = getFirestore();
     const socialGroupRef = doc(db, "socialgroups", id);
@@ -164,7 +125,6 @@ const SocialGroupsCard = ({
     if (socialGroupSnapshot.exists() && currentUser) {
       const data = socialGroupSnapshot.data();
       if (data.creator.uid === currentUser.uid) {
-        // Only allow the creator to change visibility
         const newVisibility = !data.visibility;
         await updateDoc(socialGroupRef, { visibility: newVisibility });
       } else {
@@ -189,7 +149,7 @@ const SocialGroupsCard = ({
             time,
             group,
             tag,
-            visibility, // Pass the visibility property so this is how props are passed
+            visibility,
           });
         }}
       >
@@ -200,6 +160,9 @@ const SocialGroupsCard = ({
           <Text style={styles.time}>{time}</Text>
           <Text style={styles.group}>{group}</Text>
           <Text style={styles.tag}>#{tag}</Text>
+          <Text style={styles.visibility}>
+            {visibility ? "Public" : "Private"}
+          </Text>
         </View>
       </TouchableOpacity>
       <TouchableOpacity
@@ -209,7 +172,6 @@ const SocialGroupsCard = ({
       >
         <Text style={styles.rsvpText}>RSVP</Text>
       </TouchableOpacity>
-
       <TouchableOpacity
         activeOpacity={0.7}
         onPress={handleLikes}
@@ -217,21 +179,13 @@ const SocialGroupsCard = ({
       >
         <Text style={styles.likeText}>Like {likes}</Text>
       </TouchableOpacity>
-
       <TouchableOpacity
         activeOpacity={0.7}
         onPress={toggleVisibility}
         style={styles.toggleButton}
       >
-        <TouchableOpacity
-          activeOpacity={0.7}
-          onPress={resetLikes}
-          style={styles.resetButton}
-        >
-          <Text style={styles.resetText}>Reset Likes</Text>
-        </TouchableOpacity>
         <Text style={styles.toggleText}>
-          {visibility ? "Public" : "Private"}
+          {visibility ? "Make Private" : "Make Public"}
         </Text>
       </TouchableOpacity>
       <TouchableOpacity
