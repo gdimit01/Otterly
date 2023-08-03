0a0adcee0170b4e2f6ba39860cc8d9c4414f28fe
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-08-03 13:22:42 +0100

    minor updates

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { AppNavigator } from "./infrastructure/navigation/app.navigator";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as Font from "expo-font";
 import { FontAwesome } from "@expo/vector-icons";
+import { UserProvider } from "./src/context/UserContext"; // Import UserProvider from UserContext
 
 import { LogBox } from "react-native";
 
@@ -39,7 +40,9 @@ export default function App() {
       <QueryClientProvider client={queryClient}>
         <EventProvider>
           <EventContext.Provider value={[events, setEvents]}>
-            <AppNavigator />
+            <UserProvider>
+              <AppNavigator />
+            </UserProvider>
           </EventContext.Provider>
         </EventProvider>
       </QueryClientProvider>
---
+++ src/context/UserContext.js
@@ -0,0 +1,39 @@
+// UserContext.js
+import React, { createContext, useState, useEffect } from "react";
+import { FIREBASE_AUTH as auth } from "../../firebaseConfig";
+import { getFirestore, doc, onSnapshot } from "@firebase/firestore";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [firstName, setFirstName] = useState("");
+  const [surname, setSurname] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      setUser(user);
+      if (user) {
+        const db = getFirestore();
+        const docRef = doc(db, "users", user.uid);
+        const unsubscribeDoc = onSnapshot(docRef, (docSnap) => {
+          if (docSnap.exists()) {
+            const userData = docSnap.data();
+            setFirstName(userData.firstName || "");
+            setSurname(userData.surname || "");
+          } else {
+            console.log("No such document!");
+          }
+        });
+        return unsubscribeDoc;
+      }
+    });
+    return unsubscribe;
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user, firstName, surname }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
