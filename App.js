8470ab70796963754131ab0b9a65da47820e00ff
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-08-03 11:07:49 +0100

    font update

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { EventProvider, EventContext } from "./src/context/EventContext";
 import { AppNavigator } from "./infrastructure/navigation/app.navigator";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as Font from "expo-font";
+import { FontAwesome } from "@expo/vector-icons";
 
 import { LogBox } from "react-native";
 
@@ -14,6 +16,23 @@ LogBox.ignoreLogs(["Constants.platform.ios.model has been deprecated"]);
 export default function App() {
   // Declare events state
   const [events, setEvents] = useState([]);
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      ...FontAwesome.font,
+    });
+
+    setFontLoaded(true);
+  };
+
+  if (!fontLoaded) {
+    return null; // Return null or a loading screen until fonts are loaded
+  }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
