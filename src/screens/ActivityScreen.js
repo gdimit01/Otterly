6a8657a7efd8a8b424f7af5b85ed77818e0c45ae
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-08-05 22:00:30 +0100

    better navigation 2

--- src/screens/ActivityScreen.js
+++ src/screens/ActivityScreen.js
@@ -21,80 +21,14 @@ import {
 } from "@firebase/firestore";
 import { FIREBASE_AUTH as auth } from "../../firebaseConfig";
 import NotificationCard from "../components/NotificationGroup/NotificationCard";
-import NotificationStyles from "../assets/NotificationStyles";
+import NotificationStyles from "../../src/assets/NotificationStyles";
 
-export const ActivityScreen = () => {
+const ActivityScreen = () => {
   const isFocused = useIsFocused();
   const [notifications, setNotifications] = useState([]);
   const [events, setEvents] = useContext(EventContext);
 
-  useEffect(() => {
-    if (isFocused) {
-      const db = getFirestore();
-      const user = auth.currentUser;
-      if (user) {
-        const q = query(
-          collection(db, "notifications"),
-          where("userId", "==", user.uid)
-        );
-        const unsubscribe = onSnapshot(q, (snapshot) => {
-          let notificationsData = [];
-          snapshot.forEach((doc) => {
-            notificationsData.push({ id: doc.id, ...doc.data() });
-          });
-          setNotifications(notificationsData);
-        });
-
-        return () => unsubscribe();
-      }
-    }
-  }, [isFocused]);
-
-  const deleteNotification = async (id) => {
-    const db = getFirestore();
-
-    try {
-      const notificationDoc = await getDoc(doc(db, "notifications", id));
-      const notificationData = notificationDoc.data();
-
-      await deleteDoc(doc(db, "notifications", id));
-
-      if (notificationData.eventId) {
-        const eventDoc = await getDoc(
-          doc(db, "events", notificationData.eventId)
-        );
-
-        await deleteDoc(doc(db, "events", notificationData.eventId));
-
-        setEvents(
-          (events || []).filter(
-            (event) => event.id !== notificationData.eventId
-          )
-        );
-      }
-
-      Alert.alert("Success", "Notification and event deleted!");
-
-      setNotifications(
-        (notifications || []).filter((notification) => notification.id !== id)
-      );
-    } catch (error) {
-      console.error("Error deleting document: ", error);
-    }
-  };
-
-  const renderItem = ({ item }) => (
-    <NotificationCard
-      id={item.id}
-      title={item.title}
-      description={item.description}
-      image={item.image}
-      time={item.time}
-      group={item.group}
-      tag={item.tag}
-      onDelete={() => deleteNotification(item.id)}
-    />
-  );
+  // ... rest of your code ...
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -111,3 +45,5 @@ export const ActivityScreen = () => {
     </SafeAreaView>
   );
 };
+
+export default ActivityScreen;
