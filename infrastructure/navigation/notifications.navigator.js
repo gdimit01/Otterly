2706fb0a59a1ebf48de119f2eacd253ee01e6dc5
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-08-05 01:23:42 +0100

    minor updates for top navs

--- infrastructure/navigation/notifications.navigator.js
+++ infrastructure/navigation/notifications.navigator.js
@@ -1,10 +1,28 @@
-import React from "react";
-import { SafeAreaView, View, Text, StatusBar } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  FlatList,
+  StatusBar,
+  Alert,
+} from "react-native";
+import { useIsFocused } from "@react-navigation/native";
+import { EventContext } from "../context/EventContext";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  doc,
+  getDoc,
+  deleteDoc,
+  query,
+  where,
+} from "@firebase/firestore";
+import { FIREBASE_AUTH as auth } from "../../firebaseConfig";
+import NotificationCard from "../components/NotificationGroup/NotificationCard";
+import NotificationStyles from "../assets/NotificationStyles";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import NotificationStyles from "../../src/assets/NotificationStyles";
-import { ActivityScreen } from "../screens/ActivityScreen";
-import { InvitesScreen } from "../screens/InvitesScreen";
-import { MessagesScreen } from "../screens/MessagesScreen";
 
 const Tab = createMaterialTopTabNavigator();
 
