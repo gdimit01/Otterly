add6db03673c03f41f486b64fad36353f4d4d135
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-12-01 03:58:34 +0000

    fix: homescreen back to original

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,52 +1,68 @@
+/**
+ * The HomeScreen component is a React Native screen that displays user information and a list of
+ * available events.
+ * @returns The HomeScreen component is being returned.
+ */
 import React, { useState, useEffect, useContext } from "react";
+import { FontAwesome } from "@expo/vector-icons";
 import {
+  Image,
   Text,
   SafeAreaView,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
   View,
   StatusBar,
   ActivityIndicator,
-  FlatList,
 } from "react-native";
+import { useNavigation, useIsFocused } from "@react-navigation/core";
 import { useAuth } from "../../src/hooks/useAuth";
 import styles from "../../src/assets/HomeScreen.styles";
+import { ActivityItem } from "../../src/components/ActivityItem";
 import { EventContext } from "../../src/context/EventContext";
-import GroupCard from "../../src/components/GroupCard";
+import SocialGroupsCard from "../../src/components/SocialGroups/SocialGroupsCard";
+import StudyGroupsCard from "../../src/components/StudyGroups/StudyGroupsCard";
+import { Calendar } from "react-native-calendars";
 
 export const HomeScreen = () => {
-  const { user, firstName, surname } = useAuth();
-  const { events = [] } = useContext(EventContext);
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+  const { user, firstName, surname, handleSignOut } = useAuth();
+  const { events = [] } = useContext(EventContext); // Get events from context
+
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setIsLoading(false), 500);
+    // Simulate data fetching
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+
     return () => clearTimeout(timer);
   }, []);
 
-  const filterEvents = (event) => {
-    return (
-      event.visibility ||
-      (event.creator && event.creator.email === user?.email) ||
-      (event.invites &&
-        event.invites.some(
-          (invite) =>
-            invite.email === user?.email && invite.status === "accepted"
-        ))
-    );
-  };
-
-  const renderEvent = ({ item }) => (
-    <View style={styles.groupCardContainer}>
-      <Text style={styles.eventTitle} numberOfLines={1} ellipsisMode="tail">
-        {item.title}
-      </Text>
-      <GroupCard
-        id={item.id}
-        showButtons={false}
-        showDetailsOnly={true}
-        showOptions={false}
-      />
-    </View>
+  // Combine social and study group events
+  const combinedEvents = events.filter(
+    (event) => event.group === "Social Group" || event.group === "Study Group"
   );
+
+  // Filter events based on visibility and user invitation
+  const filteredEvents = combinedEvents.filter((event) => {
+    if (event.visibility) return true;
+    if (event.creator && event.creator.email === user?.email) return true;
+
+    if (
+      event.invites &&
+      event.invites.some(
+        (invite) => invite.email === user?.email && invite.status === "accepted"
+      )
+    ) {
+      return true;
+    }
+
+    return false;
+  });
 
   if (isLoading) {
     return (
@@ -57,31 +73,72 @@ export const HomeScreen = () => {
   }
 
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent={true}
-      />
-      <View style={styles.container}>
-        <Text style={styles.title}>Home</Text>
-        {user && (
+    <SafeAreaView style={{ flex: 1, paddingTop: StatusBar.currentHeight }}>
+      {Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
+      {Platform.OS === "android" && (
+        <StatusBar
+          translucent={true}
+          backgroundColor="transparent"
+          barStyle="dark-content"
+        />
+      )}
+
+      <Text style={styles.title}>Home</Text>
+      <ScrollView contentContainerStyle={styles.container}>
+        {user ? (
           <View style={styles.userInfoContainer}>
             <Text style={styles.greetingText}>
               Hi {firstName} {surname}
             </Text>
           </View>
+        ) : (
+          <Text>No user is signed in.</Text>
         )}
         <Text style={styles.title}>Available events</Text>
-        <FlatList
-          horizontal
-          data={events.filter(filterEvents)}
-          renderItem={renderEvent}
-          keyExtractor={(item) => item.id.toString()}
-          contentContainerStyle={styles.flatListContent}
-          showsHorizontalScrollIndicator={false}
-        />
-      </View>
+        {filteredEvents.length > 0 ? (
+          <FlatList
+            horizontal
+            data={filteredEvents}
+            renderItem={({ item }) => (
+              <View style={styles.groupCardContainer}>
+                <Text
+                  style={styles.eventTitle}
+                  numberOfLines={1}
+                  ellipsizeMode="tail"
+                >
+                  {item.title}
+                </Text>
+                <View style={styles.groupCard}>
+                  {item.group === "Social Group" ? (
+                    <SocialGroupsCard
+                      id={item.id}
+                      showButtons={false}
+                      showDetailsOnly={true}
+                      showOptions={false}
+                    />
+                  ) : (
+                    <StudyGroupsCard
+                      id={item.id}
+                      showButtons={false}
+                      showDetailsOnly={true}
+                      showOptions={false}
+                    />
+                  )}
+                </View>
+              </View>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            contentContainerStyle={{ paddingBottom: 40 }}
+            showsHorizontalScrollIndicator={false}
+          />
+        ) : (
+          <View style={styles.noEventsCard}>
+            <Text style={styles.noEventsText}>
+              Looks like there are no events at the moment
+            </Text>
+          </View>
+        )}
+      </ScrollView>
     </SafeAreaView>
   );
 };
